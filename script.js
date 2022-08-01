d2964051d941f8226e65cb517c758f8facfd36d9
Rishabh <rishabh4883@gmail.com>
2022-08-01 19:39:26 +0530

    Create a function to perform the arithmetic operations
    It takes three parameters. First is the operator(string).
    The other two are operands, treated as numbers.

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+function operate(op, a, b) {
+    let result;
+    switch(op) {
+        case '+': result = add(a, b); 
+            break;
+        case '-': result = subtract(a, b); 
+            break;
+        case '*': result = multiply(a, b); 
+            break;
+        case '/': result = divide(a, b); 
+            break;
+        default: console.error("Invalid Operator!")
+    }
+    return result;
+}
