7807c72aeadc29fe643da3ed0be381b5125fdbec
Rishabh <rishabh4883@gmail.com>
2022-08-03 08:24:58 +0530

    Add two different displays for operation and result

--- script.js
+++ script.js
@@ -30,14 +30,66 @@ function operate(op, a, b) {
     return result;
 }
 
-const display = document.querySelector('.display');
+const bigDisplay = document.querySelector('.display .big');
+const smallDisplay = document.querySelector('.display .small');
 const numKeys = document.querySelectorAll('.keys .num');
+
+let num1;
+let num2;
+let operator = "";
+let numLog = "";
+let operatorCount = 0;
 
 numKeys.forEach( (key) => {
     key.addEventListener('click', updateDisplay)
 });
 
 function updateDisplay(event) {
-    let input = event.target.textContent;
-    display.textContent += input;
+    bigDisplay.textContent += event.target.textContent;
+    numLog += event.target.textContent;
 }
+
+const operatorKeys = document.querySelectorAll('.operator');
+
+operatorKeys.forEach( (key) => {
+    key.addEventListener('click', handleOperatorClick)
+});
+
+function handleOperatorClick(event) {
+    if(operatorCount) {
+        calcAndDisplay();
+        operator = event.target.textContent; 
+        bigDisplay.textContent += event.target.textContent;
+    }
+    num1 = Number(numLog);
+    numLog = "";
+    operator = event.target.textContent; 
+    bigDisplay.textContent += event.target.textContent;
+    operatorCount++;
+}
+
+const equal = document.querySelector('.equal');
+
+equal.addEventListener('click', () => {
+    num2 = Number(numLog);
+    numLog = "";
+    let result = operate(operator, num1, num2);
+    smallDisplay.textContent = bigDisplay.textContent + '=';
+    bigDisplay.textContent = result;
+});
+
+function calcAndDisplay() {
+    num2 = Number(numLog);
+    numLog = "";
+    let result = operate(operator, num1, num2);
+    smallDisplay.textContent = bigDisplay.textContent + '=';
+    bigDisplay.textContent = result;
+}
+
+const clear = document.querySelector('.clear');
+
+clear.addEventListener('click', () => {
+    numLog = "";
+    bigDisplay.textContent = "";
+    smallDisplay.textContent = "";
+});
